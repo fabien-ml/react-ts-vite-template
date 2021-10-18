25c9bcf4e51daed57a011e54f05147f5dfc3e4fe
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2021-10-18 07:43:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
-render(
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
