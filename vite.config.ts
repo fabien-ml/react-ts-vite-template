fae07069c1270fb6fb20b47ac3f71c2af03d1624
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2021-11-13 12:15:14 +0100

    feat(proxy): add vite config server proxy example

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,13 @@ export default defineConfig({
       },
     ],
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
