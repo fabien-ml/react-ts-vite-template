24a552a5692d08e57c6f672fb490da88308583c3
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2021-12-21 18:23:12 +0100

    fix: jest fail when importing jest.setup.js

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test file in the suite is executed
   // https://jestjs.io/docs/configuration#setupfilesafterenv-array
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
   // Code coverage config
   // https://jestjs.io/docs/configuration#collectcoveragefrom-array
