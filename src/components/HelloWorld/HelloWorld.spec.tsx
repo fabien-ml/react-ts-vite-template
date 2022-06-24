38fdab693e574050b0bda227df5b2bd24431fa65
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-06-24 15:38:59 +0200

    chore: bump deps

--- src/components/HelloWorld/HelloWorld.spec.tsx
+++ src/components/HelloWorld/HelloWorld.spec.tsx
@@ -5,12 +5,12 @@ import HelloWorld from "./HelloWorld";
 describe("HelloWorld", () => {
   it("should renders a msg", () => {
     // arrange
-    render(<HelloWorld msg="Hello Cypress!" />);
+    render(<HelloWorld msg="Hello React!" />);
 
     // act
     const title = screen.getByTestId("title");
 
     // assert
-    expect(title).toHaveTextContent(/Hello Cypress!/i);
+    expect(title).toHaveTextContent(/Hello React!/i);
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import legacy from "@vitejs/plugin-legacy";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import { fileURLToPath } from 'url'
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), reactRefresh()],
-  esbuild: {
-    jsxInject: `import React from 'react'`, // automatically import React in jsx files
-  },
+  plugins: [legacy(), react()],
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
-      "@": fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     proxy: {
